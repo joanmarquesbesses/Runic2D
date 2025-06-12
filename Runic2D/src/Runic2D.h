49e80c81c72e5ee9d73c25c928c669bd53f2523d
joanmarquesbesses <85459149+joanmarquesbesses@users.noreply.github.com>
2025-06-12 20:13:30 +0200

    LOG system

--- Runic2D/src/Runic2D.h
+++ Runic2D/src/Runic2D.h
@@ -2,6 +2,7 @@
 
 // For use in Runic2D applications
 #include "Runic2D/Application.h"
+#include "Runic2D/Log.h"
 
 // Entry Point
 #include "Runic2D/EntryPoint.h"
--- Runic2D/src/Runic2D/EntryPoint.h
+++ Runic2D/src/Runic2D/EntryPoint.h
@@ -6,7 +6,11 @@ extern Runic2D::Application* Runic2D::CreateApplication();
 
 int main(int argc, char** argv)
 {
-
+	Runic2D::Log::Init();
+	RUNIC2D_CORE_TRACE("Runic2D Engine Initialized!");
+	int a = 5;
+	RUNIC2D_INFO("Application started with {0} arguments", a);
+	// Initialize the application
 	auto app = Runic2D::CreateApplication();
 	app->Run();
 	delete app;
